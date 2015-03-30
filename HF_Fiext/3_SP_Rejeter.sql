5694be020afd5e6698ed8c66c9fd8f00fcd4f3e2
ABragar <Bragar_AV@ukr.net>
2015-03-30 15:30:33 +0300

    после изменений Анатолия

--- HF_Fiext/3_SP_Rejeter.sql
+++ HF_Fiext/3_SP_Rejeter.sql
@@ -50,53 +50,40 @@ and isdate(coalesce(i.DATE_MODIFICATION, '01/01/1900')) = 0
 ---Date validation
 
 --- fields not null
+
 --marque_id
 update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),25)
 from #T_Rejets i
 where FichierTS = @FichierTS and i.LigneStatut<>99 
-and IsNumeric(marque_id)=0
+-- if not numeric then -1 = bad value, the line is to be rejected
+and Cast(case when isnumeric(i.marque_id)=0 then -1 else i.marque_id end as int)  not between 1 and 9 
 
-update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),25)
-from #T_Rejets i
-where FichierTS = @FichierTS and i.LigneStatut<>99 
-and Cast(case when isnumeric(i.marque_id)=0 then 0 else i.marque_id end as int)  not between 1 and 9 
-and i.rejetCode & POWER(cast(2 as bigint),25)<>POWER(cast(2 as bigint),25)
 
 --optin-M
-update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),26)
-from #T_Rejets i
-where FichierTS = @FichierTS and i.LigneStatut<>99 
-and IsNumeric(optin_m)=0
 
 update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),26)
 from #T_Rejets i
 where FichierTS = @FichierTS and i.LigneStatut<>99 
-and Cast(case when isnumeric(i.optin_m)=0 then 0 else i.optin_m end as int)  not in (0,1) 
-and i.rejetCode & POWER(cast(2 as bigint),26)<>POWER(cast(2 as bigint),26)
+-- if not numeric then -1 = bad value, the line is to be rejected
+and Cast(case when isnumeric(i.optin_m)=0 then -1 else i.optin_m end as int)  not in (0,1) 
+
 
 --optin-p
-update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),27)
-from #T_Rejets i
-where FichierTS = @FichierTS and i.LigneStatut<>99 
-and IsNumeric(optin_p)=0
 
 update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),27)
 from #T_Rejets i
 where FichierTS = @FichierTS and i.LigneStatut<>99 
-and Cast(case when isnumeric(i.optin_p)=0 then 0 else i.optin_p end as int) not in (0,1) 
-and i.rejetCode & POWER(cast(2 as bigint),27)<>POWER(cast(2 as bigint),27)
+-- if not numeric then -1 = bad value, the line is to be rejected
+and Cast(case when isnumeric(i.optin_p)=0 then -1 else i.optin_p end as int) not in (0,1) 
 
 --source_id
-update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),28)
-from #T_Rejets i
-where FichierTS = @FichierTS and i.LigneStatut<>99 
-and IsNumeric(source_id)=0
 
 update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),28)
 from #T_Rejets i
 where FichierTS = @FichierTS and i.LigneStatut<>99 
-and Cast(case when isnumeric(i.source_id)=0 then 0 else i.source_id end as int) not in (101,102) 
-and i.rejetCode & POWER(cast(2 as bigint),28)<>POWER(cast(2 as bigint),28)
+-- if not numeric then -1 = bad value, the line is to be rejected
+and Cast(case when isnumeric(i.source_id)=0 then -1 else i.source_id end as int) not in (101,102) 
+
 
 --fichier_id
 update #T_Rejets set rejetCode = i.rejetCode | POWER(cast(2 as bigint),29)
@@ -106,9 +93,12 @@ and fichier_id is null
 
 update #T_Rejets set LigneStatut = case when RejetCode = 0 then 0 else 1 end where FichierTS = @FichierTS and LigneStatut != 99 
 
-insert into rejet.HF_Fiext select * from #T_Rejets where LigneStatut = 1 and FichierTS = @FichierTS 
-
 create unique index idx01_ImportID on #T_Rejets (ImportID)
+
+delete a
+from rejet.HF_Fiext a inner join #T_Rejets b on a.ImportID=b.ImportID
+
+insert into rejet.HF_Fiext select * from #T_Rejets where LigneStatut = 1 and FichierTS = @FichierTS 
 
 update a 
 set LigneStatut=b.LigneStatut, RejetCode=b.RejetCode
