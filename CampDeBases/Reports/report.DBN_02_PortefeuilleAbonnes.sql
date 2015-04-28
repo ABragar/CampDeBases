69d36760d183a43ae6cac6bcbc8caaca9e64ebf1
ABragar <Bragar_AV@ukr.net>
2015-04-28 16:41:20 +0300

    from Anatoly create kayak DB

--- CampDeBases/Reports/report.DBN_02_PortefeuilleAbonnes.sql
+++ CampDeBases/Reports/report.DBN_02_PortefeuilleAbonnes.sql
@@ -7,20 +7,37 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-alter proc [report].[DBN_02_PortefeuilleAbonnes] (@Appartenance int, @P nvarchar(30))
+alter proc [report].[DBN_02_PortefeuilleAbonnes] (@Editeur nvarchar(8), @P nvarchar(30))
 as 
+
+-- =============================================
+-- Author:		Anatoli VELITCHKO
+-- Creation date: 28/04/2015
+-- Description:	Calcul du Dashboard Abos Numériques 
+--				N°2
+--				PORTEFEUILLE ABONNES
+-- Modiification date :
+-- Modified by :
+-- Modification :
+-- =============================================
+
 begin 
 
 set nocount on
 
--- @Appartenance : 1 = EQ, 2 = LP, 3 = Groupe
+-- @Editeur : EQ, FF, LP
 
 declare @Period as nvarchar(30)
 declare @IdPeriod as uniqueidentifier
 declare @IdGraph as int
 declare @SnapshotDate as datetime
 declare @IdOwner as uniqueidentifier
+
 declare @IdTemplate as uniqueidentifier
+
+declare @IdTemplate_Num_EQ as uniqueidentifier
+declare @IdTemplate_Num_FF as uniqueidentifier
+declare @IdTemplate_Num_LP as uniqueidentifier
 
 declare @Progression as float
 declare @PrecPeriod as nvarchar(30)
@@ -33,23 +50,43 @@ set @Period=@P
 
 set @SnapshotDate=GETDATE()
 set @IdGraph=2
-set @IdTemplate='DC44A189-E076-4E7F-9668-B014E7AAB3B4' -- donnée de Kayak : Template DB Numérique
 
-select @IdPeriod=IdPeriode from report.RefPeriodeOwnerDB where Periode=@Period and Appartenance=@Appartenance and IdTemplate=@IdTemplate
+set @IdTemplate_Num_EQ=N'AE9B6FBA-06EF-4855-885A-BA3C2F955279'
+set @IdTemplate_Num_FF=N'9D260307-3BEF-4B0F-9B87-0BE3CE30AD3D'
+set @IdTemplate_Num_LP=N'202D2833-6EEF-449C-A2A5-509CBFB936FC'
 
-select @IdOwner=IdOwner from report.RefPeriodeOwnerDB where Periode=@Period and Appartenance=@Appartenance and IdTemplate=@IdTemplate
+set @IdTemplate=case @Editeur 
+	when N'EQ' then @IdTemplate_Num_EQ
+	when N'FF' then @IdTemplate_Num_FF
+	when N'LP' then @IdTemplate_Num_LP
+end
+
+select @IdPeriod=IdPeriode from report.RefPeriodeOwnerDB_Num where Periode=@Period and Editeur=@Editeur and IdTemplate=@IdTemplate
+
+select @IdOwner=IdOwner from report.RefPeriodeOwnerDB_Num where Periode=@Period and Editeur=@Editeur and IdTemplate=@IdTemplate
 
 declare @DebutPeriod as datetime
 select @DebutPeriod=DebutPeriod from report.RefPeriodeOwnerDB where IdPeriode=@IdPeriod
 set @PrecPeriod=N'Semaine_'+right(N'00'+cast(datepart(week,dateadd(week,-1,@DebutPeriod)) as nvarchar(2)),2)+N'_'+cast(datepart(year,dateadd(week,-1,@DebutPeriod)) as nvarchar(4))
 
-delete report.DashboardAboNumerique where Periode=@Period and IdGraph=@IdGraph and Appartenance=@Appartenance
+delete report.DashboardAboNumerique where Periode=@Period and IdGraph=@IdGraph and Editeur=@Editeur
 
 -- Abonnés numériques payants actifs
 
 ; with s as (
 select a.AbonnementID from dbo.Abonnements a
-where a.Appartenance in (1 & @Appartenance, 2 & @Appartenance)
+where a.Marque in (
+case @Editeur 
+	when N'EQ' then 7
+	when N'FF' then 3
+	when N'LP' then 6
+end
+, case @Editeur 
+	when N'EQ' then 0
+	when N'FF' then 0
+	when N'LP' then 2 -- Aujourd'hui en France
+end
+)
 )
 
 , y as (
@@ -117,7 +154,7 @@ select @Period as Periode
 , @IdTemplate as IdTemplate
 , @SnapshotDate as SnapshotDate
 , @IdGraph as IdGraph
-, @Appartenance as Appartenance
+, @Editeur as Appartenance
 , t.Libelle as Libelle
 , t.NumOrder
 , t.NombreActifs as ValeurFloat 
