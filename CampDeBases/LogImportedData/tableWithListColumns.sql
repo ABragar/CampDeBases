6b663351bf7cf7caeebc1f7d1eac0fa95ba3ffc4
ABragar <Bragar_AV@ukr.net>
2015-05-26 15:57:22 +0300

    save projects change

--- CampDeBases/LogImportedData/tableWithListColumns.sql
+++ CampDeBases/LogImportedData/tableWithListColumns.sql
@@ -1,5 +1,6 @@
-
-SELECT Table_Name
+SELECT COUNT(*), Fields
+FROM
+(SELECT Table_Name
       ,(
            SELECT Column_name + ','
            FROM   information_schema.columns t2
@@ -11,5 +12,14 @@ FROM   information_schema.columns t1
 WHERE  TABLE_SCHEMA = N'import'
        AND COLUMN_NAME IN ('RejetCode' ,'FichierTS' ,'ActionID' ,'TIMESTAMP')
 GROUP BY
-       Table_Name
+       Table_Name) xxx
+GROUP BY Fields
 ORDER BY Fields
+
+
+select * from import.PROB_Bounces
+select * from rejet.PROB_Bounces
+ActionID,FichierTS,RejetCode,
+
+select * from import.LPPROSP_Prospects
+select * from rejet.LPPROSP_Prospects
