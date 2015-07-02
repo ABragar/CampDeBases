9b7c69bc118c25037a7686b0e70deac3951ffb44
ABragar <Bragar_AV@ukr.net>
2015-07-02 13:35:37 +0300

    Create, input data

---
+++ CampDeBases/Typologies/Ajout Ref Typos.sql
@@ -0,0 +1,21 @@
+use AmauryVUC
+go
+
+-- Terminés
+insert ref.Misc (TypeRef,CodeValN,Valeur)
+values (N'TYPOLOGIE',82,N'CSPGT - Clients souscripteurs papier gratuits terminés')
+, (N'TYPOLOGIE',83,N'CSPPT - Clients souscripteurs papier payants terminés')
+, (N'TYPOLOGIE',84,N'CSNGT - Clients souscripteurs numérique gratuits terminés')
+, (N'TYPOLOGIE',85,N'CSNGT - Clients souscripteurs numérique payants terminés')
+go
+
+-- Visiteurs identifiés
+insert ref.Misc (TypeRef,CodeValN,Valeur)
+values (N'TYPOLOGIE',86,N'VIMN - Visiteurs identifiés marque nouveaux')
+, (N'TYPOLOGIE',87,N'VIMA - Visiteurs identifiés marque actuels')
+, (N'TYPOLOGIE',88,N'VIMI - Visiteurs identifiés marque inactifs')
+, (N'TYPOLOGIE',89,N'VIEN - Visiteurs identifiés éditeur nouveaux')
+, (N'TYPOLOGIE',90,N'VIEA - Visiteurs identifiés éditeur actuels')
+, (N'TYPOLOGIE',91,N'VIEI - Visiteurs identifiés éditeur inactifs')
+go
+
