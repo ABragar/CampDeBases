a05543e4b04a7c479d8ed2496c31304f7f1445e0
ABragar <Bragar_AV@ukr.net>
2015-08-18 16:11:50 +0300

    etl.SupprDoublonsConsent procedure

---
+++ CampDeBases/Common/etl.SupprDoublonsConsent.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE etl.SupprDoublonsConsent
+AS
+BEGIN
+	SET NOCOUNT ON
+	;
+	WITH doubles AS (
+	         SELECT ConsentementID
+	         FROM   (
+	                    SELECT ConsentementID
+	                          ,ROW_NUMBER() OVER(
+	                               PARTITION BY Email
+	                              ,ContenuID
+	                              ,MasterID 
+	                               ORDER BY ConsentementDate DESC
+	                              ,valeur ASC
+	                           ) N
+	                    FROM   dbo.ConsentementsEmail
+	                ) x
+	         WHERE  N > 1
+	     )
+	
+	DELETE c
+	FROM   dbo.ConsentementsEmail c
+	       INNER JOIN doubles d
+	            ON  c.ConsentementID = d.ConsentementID
+END
+
+
